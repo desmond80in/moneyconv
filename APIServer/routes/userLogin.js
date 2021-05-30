1e9d07464b6897d280d4e20457b728bab10f7b7d
Desmond Fernando <Desmond.Fernando@NMG-Group.com>
2021-05-31 06:09:31 +0800

    Updated login and web

--- APIServer/routes/userLogin.js
+++ APIServer/routes/userLogin.js
@@ -1,24 +1,40 @@
-const crypto = require('crypto');  
-import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+const crypto = require("crypto");
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
 
-export default async(req,res,redis) => {  
+export default async (req, res, redis) => {
   const user = req.body;
-  if(!user.password || !user.username ){
-    return res.status(400).send({message: 'Invalid username or password'});
+
+  if (!user.password || !user.username) {
+    return res.status(400).send({
+      isAuth: false,
+      message: "Invalid username or password",
+    });
   }
 
   let userResult = await redis.get(user.username);
-  if(!userResult){
-    return res.status(400).send({message: 'Invalid username or password'});
+  if (!userResult) {
+    return res.status(400).send({
+      isAuth: false,
+      message: "Invalid username or password",
+    });
   }
 
-  var hPassword = crypto.createHash('md5').update(user.password).digest("hex");
+  var hPassword = await crypto
+    .createHash("md5")
+    .update(user.password)
+    .digest("hex");
 
-  if(hPassword === userResult.password){
-    return res.status(400).send({message: 'Invalid username or password'});
-  }  
+  if (hPassword.toString() !== userResult.toString()) {
+    return res.status(400).send({
+      isAuth: false,
+      message: "Invalid username or password",
+    });
+  }
 
-  jwt.sign({ user }, 'secretKey', { expiresIn: 60 * 60 },( err, token) => {
-    res.json({ token });
+  const token = jwt.sign({ user }, "secretKey", { expiresIn: 60 * 60 });
+
+  res.status(200).send({
+    isAuth: true,
+    token,
   });
-}
+};
--- APIServer/server.js
+++ APIServer/server.js
@@ -1,73 +1,74 @@
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import { ApolloServer, gql,AuthenticationError } from 'apollo-server-express';
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import express from "express";
+import bodyParser from "body-parser";
+import { ApolloServer, gql, AuthenticationError } from "apollo-server-express";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 import { makeExecutableSchema } from "graphql-tools";
-import userRegistration from './routes/userRegistration';
-import userLogin from './routes/userLogin';
+import userRegistration from "./routes/userRegistration";
+import userLogin from "./routes/userLogin";
 
-const {Auth,validateRoute} = require('./auth');
-const Redis = require('ioredis');
-const crypto = require('crypto');
-const secretKey =  'secretKey';
+const { Auth, validateRoute } = require("./auth");
+const Redis = require("ioredis");
+const crypto = require("crypto");
+const cors = require("cors");
+const secretKey = "secretKey";
+
 export const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
 const PORT = 4000;
-const path = '/graphiql';
+const path = "/graphiql";
 
 const app = express();
 app.use(express.json());
 
-
-
+////////////////////////////////////////// -----Cros----- ///////////////////////////////////////////
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors());
 
 /////////////////////////////////////////// ----Redis---- ///////////////////////////////////////////
 
 const redis = new Redis({
-    host: 'redis-19760.c251.east-us-mz.azure.cloud.redislabs.com',
-    port: 19760,
-    password: 'E0WRtfau47LclMRSMVA6MyF36d1GB4i8'
+  host: "redis-19760.c251.east-us-mz.azure.cloud.redislabs.com",
+  port: 19760,
+  password: "E0WRtfau47LclMRSMVA6MyF36d1GB4i8",
 });
 
 /////////////////////////////////////////// ----Redis---- ///////////////////////////////////////////
 
-
-
 /////////////////////////////////////////// ----JWT---- ///////////////////////////////////////////
 
-app.post('/api/register', async(req,res, next)=> {
-  await userRegistration(req,res,redis);
+app.post("/api/register", async (req, res, next) => {
+  await userRegistration(req, res, redis);
   next();
-})
+});
 
 // login
-app.post('/api/login',async (req,res, next)=> {
-  await userLogin(req,res,redis);
+app.post("/api/login", async (req, res, next) => {
+  await userLogin(req, res, redis);
   next();
-})
+});
 /////////////////////////////////////////// ----JWT---- ///////////////////////////////////////////
 
 /////////////////////////////////////////// ----gQL---- ///////////////////////////////////////////
 
-const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers,
-    context: ({req})=>{
-      const authReq = Auth(req);
-      return {auth: authReq, redis}
-    },
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const authReq = Auth(req);
+    return { auth: authReq, redis };
+  },
 });
 
 server.applyMiddleware({ app, path });
 
-
 /////////////////////////////////////////// ----gQL---- ///////////////////////////////////////////
 app.listen({ port: PORT }, () =>
   console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
-)
-        
+);
